0259d9137a63d5df3a54bcbfc112be2e81a60744
sbraunstein <sbraunstein12@gmail.com>
2015-08-13 15:51:01 -0500

    basic functionality working. Need to add features

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,57 +11,51 @@ angular.module('cagefightApp')
   .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
   	$scope.movie = {};
   	$scope.points = 0;
+  	$scope.moviePoster = {};
   	$scope.randomInt = function(min, max) {
   		return Math.floor(Math.random() * (max - min)) + min;
   	};
+  	$scope.yesMovieInfo = function (obj) {
+  		for(var i = 0; i < obj.cast.length; i++){
+  			if(obj.cast[i].title === $scope.movie.title){
+	  				$scope.points++;
+	  				return $scope.getMovie();
+  			}
+  		}
+  	};
+  	$scope.noMovieInfo = function (obj) {
+  		for(var i = 0; i < obj.cast.length; i++){
+  			if(obj.cast[i].title === $scope.movie.title){
+  				return $scope.getMovie();
+  			}
+  		}
+  		$scope.points++;
+  		return $scope.getMovie();
+  	};
   	$scope.yes = function () {
-  		var baseURL = 'http://api.themoviedb.org/3/person/';
+  		var URL = 'http://api.themoviedb.org/3/person/2963/movie_credits?api_key=a83e28dc44baf347eb09a87d44ee2885';
 		console.log(' MovieService.js, Making request');
-		var apiKey = 'a83e28dc44baf347eb09a87d44ee2885';
-		return $http.get(baseURL + '2963' + 'movie_credits' + '?api_key=' + apiKey)
+		return $http.get(URL)
 			.success(function(response) {
-				if($scope.movie === response.title) {
-					$scope.points = $scope.points++
-					return $scope.getMovie();
-				};
-				else {
-					return $scope.getMovie();
-				}
-			})
+					console.log('yes function', response)
+				return $scope.yesMovieInfo(response);
+			});
   	};
   	$scope.no = function () {
-  		var baseURL = 'http://api.themoviedb.org/3/person/';
+  		var URL = 'http://api.themoviedb.org/3/person/2963/movie_credits?api_key=a83e28dc44baf347eb09a87d44ee2885';
 		console.log(' MovieService.js, Making request');
 		var apiKey = 'a83e28dc44baf347eb09a87d44ee2885';
-		return $http.get(baseURL + '2963' + 'movie_credits' + '?api_key=' + apiKey)
+		return $http.get(URL)
 			.success(function(response) {
-				if($scope.movie !== response.title) {
-					$scope.points = $scope.points++
-					return $scope.getMovie();
-				};
-				else {
-					return $scope.getMovie();
-				}
+				console.log('no function', response.cast[0].title)
+				return $scope.noMovieInfo(response);
 			})
   	};
   	$scope.youWin = function () {
   		if($scope.points === 15) {
-  			alert('You Win!');
+  			$scope.win === 'You Win!';
   		}
-  	}
-
-   	// $scope.productionTitle = function(obj) {
-  	// 	for(var prop in obj) {
-  	// 		if(obj.hasOwnProperty(prop)) {
-  	// 			if(obj[prop] === "Metro-Goldwyn-Mayer (MGM)" || "Warner Bros Pictures" || "Paramount Pictures" || "Walt Disney Pictures" ||  "New Line Cinema" || "Lions Gate Films" || "Columbia Pictures Corporation" || "Touchstone Pictures" || "TriStar Pictures" || "Universal Pictures" || "Warner Bros." || "Touchstone Pictures" || "Warner Bros. Pictures" || "Dimension Films" || "20th Century Fox") {
-  	// 				return true;
-  	// 			}
-  	// 			else {
-  	// 				return false;
-  	// 			}
-  	// 		}
-  	// 	}
-  	// };
+  	};
   	$scope.getMovie = function () {
   		var baseURL = 'http://api.themoviedb.org/3/movie/';
 		console.log(' MovieService.js, Making request');
@@ -70,45 +64,21 @@ angular.module('cagefightApp')
 		return $http.get(baseURL + id + '?api_key=' + apiKey)
   			.success(function(response){
 		// console.log("Response:", response)
-				console.log(response);
 				if((Number(response.release_date.split("").splice(0,4).join("")) > 1981) && (response.spoken_languages[0].name === 'English')){
 					// console.log("response: ", response)
 					console.log("success")
 					$scope.movie = response;
+					$scope.moviePoster = response.poster_path;
 					console.log('$scope.movie', $scope.movie);
 					return response;
 				} else {
 					return $scope.getMovie();
 					console.log("before 1981")
 				}
-				})
-			  .error(function(e){
+			})
+		    .error(function(e){
 				console.log("error")
 				return $scope.getMovie();
 			})
 		};
-
-
-
-
-  // 	function getRandomMovie(item) {
-  // 		console.log('main.js, getRandomMovie', GrabMovie);
-  // 		GrabMovie.getRandomMovie()
-  // 			.success(function(response){
-		// // console.log("Response:", response)
-		// 		console.log(response)
-		// 		if(Number(response.data.release_date.split("").splice(0,4).join("")) > 1981){
-		// 			// console.log("response: ", response)
-		// 			console.log("success")
-		// 			return response;
-		// 		} else {
-		// 			return exports.getRandomMovie();
-		// 			console.log("before 1981")
-		// 		}
-		// 	})
-  // 		.error(function(e){
-		// 	console.log("error")
-		// 	return exports.getRandomMovie();
-		// })
-  // 	};
   }]);
